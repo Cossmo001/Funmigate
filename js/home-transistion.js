09ede3cf58d78418c43301e916bfbc8185ba4992
praiseolotu <olioxxayo@gmail.com>
2023-11-29 03:27:19 +0100

    setup js folder

--- js/home-transistion.js
+++ js/home-transistion.js
@@ -1,10 +1,12 @@
 var changingText1 = document.getElementById('other');
+var colorizer = document.getElementById('colorizer');
 var word = ['Home', 'Office', 'Religious Center', 'Event', ];
 var indexx = 0;
 
 function changeeText() {
     changingText1.textContent = `Make your ${word[indexx]} pest free for as low as ₦5000`;
-    changingText1.style.color = index % 2 === 0 ? '#FDF001' : 'white';
+    //changingText1.style.color = index % 2 === 0 ? '#FDF001' : 'white';
+    colorizer.style.color = 'yellow';
     indexx = (indexx + 1) % word.length;
 }
 
--- js/pest.js
+++ js/pest.js
@@ -1,10 +1,13 @@
 const changingText = document.getElementById('changingText');
+const colorize = document.getElementById('colorize');
 const words = ['Bed Bugs', 'Cockroaches', 'Rat', 'Flies', 'Ant', 'Fleas', 'Bees', 'Moths'];
 let index = 0;
 
 function changeText() {
-    changingText.textContent = `Control ${words[index]} at your home for as low as ₦5000`;
-    changingText.style.color = index % 2 === 0 ? '#FDF001' : 'white';
+    changingText.innerText = `Control ${words[index]} at your home for as low as ₦5000`;
+    //changingText.style.color = index % 2 === 0 ? '#FDF001' : '#ffff11';
+    //colorize.style.color = '#ffff11';
+    colorize.style.color = index % 2 === 0 ? '#FDF001' : '#ffff11';
     index = (index + 1) % words.length;
 }
 
